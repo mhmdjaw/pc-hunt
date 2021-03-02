15220bc955a3d6e5976ead14938c85ff5b48358f
mhmd jawhar <mohamad.jaouhar98@gmail.com>
2021-03-02 15:34:48 -0500

    extend user interface in express

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -3,6 +3,14 @@ import { IProduct } from "../../src/models/product";
 
 declare global {
   namespace Express {
+    interface User {
+      id: string;
+      name: string;
+      email: string;
+      about?: string;
+      role: number;
+      history: Array<string>;
+    }
     interface Request {
       product: IProduct;
       category: ICategory;
