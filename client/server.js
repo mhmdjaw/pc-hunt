7804d99650ba0fcf43c022455a805200e72005af
mhmd jawhar <mohamad.jaouhar98@gmail.com>
2021-04-25 21:22:54 -0400

    fix server.js bug

--- client/server.js
+++ client/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import compression from "compression";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(compression());
